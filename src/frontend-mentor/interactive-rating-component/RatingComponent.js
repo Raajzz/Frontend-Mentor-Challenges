890fe20f5cd051868a6a67b1ba34230168e01a35
Raajzz <raajkumar.s2020@vitstudent.ac.in>
2022-05-31 22:40:25 +0530

    Added favicon and button
    Added states to the submit button, now to work on adding the review buttons and we need to add react-router-dom in addition for the other page

--- src/frontend-mentor/interactive-rating-component/RatingComponent.js
+++ src/frontend-mentor/interactive-rating-component/RatingComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import "./RatingComponent.css";
+import iconStar from "./assets/icon-star.svg";
 
 const RatingComponent = () => {
 	return (
@@ -18,14 +19,18 @@ const RatingComponent = () => {
 			</Helmet>
 			<div className="card-container">
 				<div className="card">
+					<div className="icon-container">
+						<img src={iconStar} alt="star icon" className="icon" />
+					</div>
 					<h1 className="question">How did we do?</h1>
 					<p className="request">
 						Please let us know how we did with your support reqest. All feedback
 						is appreciated to help us imporve our offering!
 					</p>
-					<div className="rating">
-						
-					</div>
+					<div className="rating"></div>
+					<button className="submit-btn">
+						SUBMIT
+					</button>
 				</div>
 			</div>
 		</>
