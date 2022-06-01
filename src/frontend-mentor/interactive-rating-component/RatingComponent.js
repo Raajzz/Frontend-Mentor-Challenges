e4fb895a54ab959487091524128b592c003192bd
Raajzz <raajkumar.s2020@vitstudent.ac.in>
2022-06-01 12:34:53 +0530

    Completed the rating page

--- src/frontend-mentor/interactive-rating-component/RatingComponent.js
+++ src/frontend-mentor/interactive-rating-component/RatingComponent.js
@@ -22,15 +22,21 @@ const RatingComponent = () => {
 					<div className="icon-container">
 						<img src={iconStar} alt="star icon" className="icon" />
 					</div>
-					<h1 className="question">How did we do?</h1>
-					<p className="request">
-						Please let us know how we did with your support reqest. All feedback
-						is appreciated to help us imporve our offering!
-					</p>
-					<div className="rating"></div>
-					<button className="submit-btn">
-						SUBMIT
-					</button>
+					<div className="qn-req-container">
+						<h1 className="question">How did we do?</h1>
+						<p className="request">
+							Please let us know how we did with your support reqest. All
+							feedback is appreciated to help us imporve our offering!
+						</p>
+					</div>
+					<ul>
+						<li>1</li>
+						<li>2</li>
+						<li>3</li>
+						<li>4</li>
+						<li>5</li>
+					</ul>
+					<button className="submit-btn">SUBMIT</button>
 				</div>
 			</div>
 		</>
