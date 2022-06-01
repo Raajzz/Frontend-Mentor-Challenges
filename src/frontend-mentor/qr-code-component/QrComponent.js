039c2061cbdb7b0f5c5b1e8d794debc851afdcc9
Raajzz <raajkumar.s2020@vitstudent.ac.in>
2022-06-02 00:00:40 +0530

    Completed QR Component

--- src/frontend-mentor/qr-code-component/QrComponent.js
+++ src/frontend-mentor/qr-code-component/QrComponent.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Helmet from "react-helmet";
-import "./QrComponent.css"
-import qrCodeImage from "./assets/image-qr-code.png"
+import "./QrComponent.css";
+import qrCodeImage from "./assets/image-qr-code.png";
 
 const QrComponent = () => {
 	return (
@@ -12,17 +12,22 @@ const QrComponent = () => {
           
             body{
               background-color: hsl(212, 45%, 89%);
+							font-family: 'Outfit', sans-serif;
             }
 
           `}
 				</style>
 			</Helmet>
 			{/* CARD */}
-      <div className="card-container">
-        <div className="card">
-          <img src={qrCodeImage} alt="" />
-        </div>
-      </div>
+			<div className="card-container">
+				<div className="card">
+					<img src={qrCodeImage} alt="" />
+					<div className="text-part">
+						<h1>Improve your front-end skills by building projects</h1>
+						<p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 };
